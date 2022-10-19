0fa047df60041a11ceccf4921bb1f3124b65bd03
AndresBerdu <101230318+AndresBerdu@users.noreply.github.com>
2022-10-19 11:07:49 -0500

    Método de prueba
    se agrego un método de prueba

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Go!")
+import express  from 'express'
+import {USER_BBDD} from './DataBase/USERS_BBDD'
+
+const accountRouter = express.Router();
+
+accountRouter.use((req, res, next)=> {
+    console.log(req.ip);
+    next()
+})
+
+accountRouter.post('', (req, res) => {
+    const { name, guid }  = req.body
+
+    if (!name || !guid)  return res.status(400).send();
+     
+        const user = USER_BBDD.find( name => name.guid === guid)
+        
+        if(name) return res.status(409).send()
+        
+        USER_BBDD.push({
+            name, guid
+    })
+
+    return res.send()
+})
